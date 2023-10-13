cbeb26f961642406d731a2e94ca6cf1680d965ca
Evan Wright <emw8105@gmail.com>
2023-10-13 17:50:43 -0500

    File and Line Condensing, WIP of optimization
    Brian has challenged me to complete the whole program in under 13 lines, he managed to do it in 4, further research is required regarding Axios.any for concurrent requests

--- DDLB/leaderboardAPI.js
+++ DDLB/leaderboardAPI.js
@@ -1,36 +1,40 @@
 // create a leaderboard output that prints the rank, id, time, and death type of each entry in the returned JSON
 // https://devildaggers.info/swagger/index.html#/Leaderboards/Leaderboards_GetLeaderboard
 
-// NEED TO: research logging for implementation as an AWS lambda
-
-const axios = require('axios')
-const http = require('http')
-
-console.log('running')
-
+// NEED TO: research logging for implementation as an AWS lambda, optimize to not use await calls for every API response
 // first implementation, too slow due to awaits
 
-const getLeaderboard = async(start) => {
-    let res = await axios({
-        mehod: 'get',
-        url: `https://devildaggers.info/api/leaderboards?rankStart=${start}`,
-    })
-    .then(response => {
-        entries = response.data.entries
+const axios = require('axios')
+console.log('running')
 
-        for (let i = 0; i < entries.length; i++) {
-            entry = entries[i]
-            console.log(`Rank: ${entry.rank}`),
-            console.log(`id: ${entry.id}`),
-            console.log(`Time: ${entry.time}`) 
-            console.log(`Death Type: ${entry.deathType}`) 
-            console.log('\n')
-        }
-        return entries.length
+const getLeaderboard = async(start) => {
+    let res = await axios.get(`https://devildaggers.info/api/leaderboards?rankStart=${start}`)
+    .then(response => {
+        return response.data.entries
     })
     return res
 }
-
-module.exports = {
-    getLeaderboard
+const getTotalPlayers = async(start) => {
+    return await axios.get(`https://devildaggers.info/api/leaderboards?rankStart=1`)
+    .then(response => {
+        return response.data.totalPlayers
+    })
 }
+
+const wrapperFunc = async () => {
+    let start = 1, entries, totalPlayers = await getTotalPlayers()
+    console.log(totalPlayers)
+    while(start <= totalPlayers) {
+        entries = getLeaderboard(start)
+        for (let i = 0; i < entries.length; i++) {
+            entry = entries[i]
+            console.log(`Rank: ${entry.rank}\nid: ${entry.id}\nTime: ${entry.time}\nDeath Type: ${entry.deathType}\n\n`)
+        }
+        start += 100
+    }
+}
+wrapperFunc()
+
+//axios.all
+//iife
+//get total players from stats call rather than awaiting each call to check if entities length = 0
