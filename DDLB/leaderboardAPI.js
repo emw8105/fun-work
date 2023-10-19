d60c4eefc5a36030d20bea71f31f882e84a84bb0
Evan Wright <emw8105@gmail.com>
2023-10-19 15:29:07 -0500

    Runtime Optimization through Axios.All
    Requests are processed in batches of 10000 players, or 100 URLs requests. I attempted to do it all requests simultaneously but the Devil Dagger server was not happy with that and rejected my call. Could be optimized further, and I have been challenged to reduce the line count further although with my current implementation it would be possible but uglier. Still a WIP

--- DDLB/leaderboardAPI.js
+++ DDLB/leaderboardAPI.js
@@ -2,39 +2,25 @@
 // https://devildaggers.info/swagger/index.html#/Leaderboards/Leaderboards_GetLeaderboard
 
 // NEED TO: research logging for implementation as an AWS lambda, optimize to not use await calls for every API response
-// first implementation, too slow due to awaits
 
 const axios = require('axios')
 console.log('running')
 
-const getLeaderboard = async(start) => {
-    let res = await axios.get(`https://devildaggers.info/api/leaderboards?rankStart=${start}`)
-    .then(response => {
-        return response.data.entries
-    })
-    return res
-}
-const getTotalPlayers = async(start) => {
-    return await axios.get(`https://devildaggers.info/api/leaderboards?rankStart=1`)
-    .then(response => {
-        return response.data.totalPlayers
-    })
-}
+const getTotalPlayers = async() => { // gets the current total player count to iterate until to print the whole leaderboard
+    return await axios.get(`https://devildaggers.info/api/leaderboards?rankStart=1`).then(response => { return response.data.totalPlayers})}
 
-const wrapperFunc = async () => {
-    let start = 1, entries, totalPlayers = await getTotalPlayers()
-    console.log(totalPlayers)
-    while(start <= totalPlayers) {
-        entries = getLeaderboard(start)
-        for (let i = 0; i < entries.length; i++) {
-            entry = entries[i]
+(async () => {
+    let start = 1, urls = [], end = 10000, playerCount = await getTotalPlayers()
+    console.log(`Total players: ${playerCount}`)
+    while(end <= playerCount) { // server doesnt like thousands of simultaneous requests, have to send chunks of requests
+        while(start < end) {urls.push(`https://devildaggers.info/api/leaderboards?rankStart=${start}`), start+=100} // push next 100 URL requests onto array representing next 10000 players
+        const requests = urls.map((url) => axios.get(url)); // map url requests to axios.get so requests can be made simultaneously with axios.all
+        let res = await axios.all(requests)
+        res.forEach(response => { // loop through JSON response and print entry information to populate the leaderboard
+            response.data.entries.forEach(entry => {
             console.log(`Rank: ${entry.rank}\nid: ${entry.id}\nTime: ${entry.time}\nDeath Type: ${entry.deathType}\n\n`)
-        }
-        start += 100
+            })
+        })
+        end += 10000, urls = [] // update to get next 10000 players
     }
-}
-wrapperFunc()
-
-//axios.all
-//iife
-//get total players from stats call rather than awaiting each call to check if entities length = 0
+})()
